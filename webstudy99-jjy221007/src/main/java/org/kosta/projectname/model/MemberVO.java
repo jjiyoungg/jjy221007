5b694e4c3895e012209be305e4f59b051872a4f1
jjiyoungg <jh99165@naver.com>
2022-10-07 15:06:58 +0900

    메인 ui 2 작업 및 회원 name 추가

--- webstudy99-jjy221007/src/main/java/org/kosta/projectname/model/MemberVO.java
+++ webstudy99-jjy221007/src/main/java/org/kosta/projectname/model/MemberVO.java
@@ -2,12 +2,18 @@ package org.kosta.projectname.model;
 
 public class MemberVO {
 	private String id;
-
+	private String name;
 	public String getId() {
 		return id;
 	}
 	public void setId(String id) {
 		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
 	}
 	
 }
